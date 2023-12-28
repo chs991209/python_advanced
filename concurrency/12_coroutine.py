2d8958949a7ab17507f7e97075f8ce73ae0de35a
ChoiHs <chs991209@gmail.com>
2023-12-28 11:16:11 +0900

    ADD: Concurrency 46번까지 간단한 잘못된 부분 수정

--- concurrency/12_coroutine.py
+++ concurrency/12_coroutine.py
@@ -7,9 +7,10 @@
 # sub routine은 main routine에서 return에 의해서, sub routine call 부분으로 돌아와 다시 process 실행
 # Coroutine : routine 실행 중 멈춤 가능 -> 특정 위치로 돌아갔다가 -> 다시 원래 위치로 돌아와 process 실행 -> 탈출부를 나가지 않고 분할해서 실행 가능
 # -> 동시성 프로그래밍 가능화
+# Scheduling Overhead (스케줄링 오버헤드)와 같은 서버의 메모리 낭비가 매우 적음.
 # 단점: 가독성이 떨어짐
 # threading : Single -> Multi -> Complex -> Shared resource -> Stuck status 발생 가능성
-# ex. PC의 멀티 작업
+# ex. 브라우저의 멀티 threads 작업
 # context switching 비용 발생, resource 소비 가능성 증가
 
 # Coroutine의 Basic Structure -> Coroutine의 단점 체크
@@ -43,6 +44,7 @@ print("EX 1-1 - ", c1, type(c1))
 
 from inspect import getgeneratorstate
 
+
 def coroutine2(x):
     print(">>> coroutine began : {}".format(x))
     y = yield x
@@ -74,7 +76,8 @@ def coroutine(func):
     :param func:
     :return:
     """
-    @wraps(func)    # 주석 등을 가져옴
+
+    @wraps(func)  # 주석 등을 가져옴
     def primer(*args, **kwargs):
         gen = func(*args, **kwargs)
         next(gen)
